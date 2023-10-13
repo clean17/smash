e0dd54b0f0b93a8621cbc96a306a2296486d9dc8
clean17 <piw940317@gmail.com>
2023-10-14 01:32:27 +0900

    ãabort

--- central-server/main/java/com/example/springbreaking/other/tempPersistence/TempMapper.java
+++
@@ -1,9 +0,0 @@
-package com.example.springbreaking.other.tempPersistence;
-
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface TempMapper {
-
-
-}
--- central-server/main/java/com/example/springbreaking/servlet/caching/BookRepository.java
+++
@@ -1,6 +0,0 @@
-package com.example.springbreaking.servlet.caching;
-
-public interface BookRepository {
-  Book getByIsbn(String isbn);
-
-}
