27998d7c08e014f92ef05c9abef3806f15629d82
clean17 <piw940317@gmail.com>
2023-10-30 23:13:04 +0900

    TIL-64

--- src-client/src/main/java/com/example/multimodule/other/AsyncController.java
+++ src-client/src/main/java/com/example/multimodule/other/AsyncController.java
@@ -3,6 +3,11 @@ package com.example.multimodule.other;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Controller;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 /**
@@ -50,4 +55,37 @@ public class AsyncController {
         // 비동기로 실행할 로직
         System.out.println("Executing async method");
     }
+
+    //////////////////////// CompletableFuture + ExecutorService
+    ExecutorService executor = Executors.newFixedThreadPool(17);
+//    CompletableFuture<Void> future = CompletableFuture.runAsync(() -> {
+//        strList.parallelStream().forEach((item) -> {
+//            List<String> tempList = new ArrayList<>();
+//            int tot = 0;
+//            try (BufferedReader br = new BufferedReader(new FileReader(item))) {
+//                String line;
+//                while ((line = br.readLine()) != null) {
+//                    tempList.add(line);
+//                    if(tempList.size() == 1000) {
+//                        tot += tempList.size();
+//                        tempList.clear();
+//                    }
+//                }
+//            } catch (IOException e) {
+//                e.printStackTrace();
+//            }
+//            synchronized (System.out) {
+//                System.out.println("File: " + item + ", Line Count: " + tot);
+//            }
+//        });
+//    }, executor).thenAccept(future ->{
+//        // 비동기 끝나면 실행
+//        try {
+//            // 비동기 작업이 완료될 때까지 대기
+//            future.get();
+//        } catch (InterruptedException e) {
+//            e.printStackTrace();
+//        }
+//    });
+
 }
